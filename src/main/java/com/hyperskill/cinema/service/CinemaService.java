22007392847950692c0ed3a029fd1e55280c3869
Elnur Kasimov <ekasimov2012@gmail.com>
2023-11-14 14:05:08 +0200

    created package 'service' and interface CinemaService and class CinemaServiceImpl

---
+++ src/main/java/com/hyperskill/cinema/service/CinemaService.java
@@ -0,0 +1,13 @@
+package com.hyperskill.cinema.service;
+
+
+
+import com.hyperskill.cinema.model.Seat;
+
+import java.util.List;
+
+public interface CinemaService {
+    int getRowQuantity();
+    int getColumnQuantity();
+    List<Seat> getAllSeats();
+}
---
+++ src/main/java/com/hyperskill/cinema/service/implementation/CinemaServiceImpl.java
@@ -0,0 +1,25 @@
+package com.hyperskill.cinema.service.implementation;
+
+
+
+import com.hyperskill.cinema.model.Seat;
+import com.hyperskill.cinema.service.CinemaService;
+
+import java.util.List;
+
+public class CinemaServiceImpl implements CinemaService {
+    @Override
+    public int getRowQuantity() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnQuantity() {
+        return 0;
+    }
+
+    @Override
+    public List<Seat> getAllSeats() {
+        return null;
+    }
+}
